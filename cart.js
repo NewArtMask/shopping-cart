d2c9adbcc278ad81931fc8e207abfd811d4a5de9
NewArtMask <34275837+NewArtMask@users.noreply.github.com>
2020-06-26 22:57:59 +0200

    main questions ansvered
    An Add to Cart Button for each item
    Display the items in the shopping cart.
    Display the subtotal, tax, and total (you can assume just one percentage like 10% for tax no matter what).
    Display a form for the user to enter their Shipping Information.
    A Submit button that will alert that the order has been sent (note, nothing is actually sent, just testing it does something when clicked.
    Calculate total tax

--- cart.js
+++ cart.js
@@ -1,24 +1,121 @@
-	//const orderItems = [];
-	const items = {};
+	let orderItems = [];//all goods in a shopping card
+	const items = {};//item from a product card
+	$('.cart').append($('<div>').addClass('cart-items'));
 
+//get name and price from a shopping card
 $('.waren button').click(function () {
-	let p = $(this).parent().find('p');
+	let p = $(this).siblings('p');
 	items['name'] = p.eq(0).text();
 	items['price'] = p.eq(1).text();
-	//orderItems.push(items);
-	shoppingCard(items);
-	//console.log(...orderItems);
+	orderItems.push(items);//add to all goods array
+	shoppingCard(items);//transfer dara to shopping card
 });
 
 function shoppingCard (items) {
-	let div = $('<div>').addClass('cardItems').appendTo($('.cart'));
+	//add bootstrap 4 card
+	//<div> class = 'card' append
+	//<div> class = 'card-body' append
+	//<h5> class = 'card-title' and
+	//<p> class = 'card-text' and
+	//<a> class = 'btn btn-danger' with html text
+	$('.cart-items')
+		.append($('<div>').addClass('card')
+		.append($('<div>').addClass('card-body')
+		.append($('<h5>').addClass('card-title'), 
+				$('<p>').addClass('card-text'), 
+				$('<a>').addClass('btn btn-danger').html('Delete item'))));
 
-	let price = parseFloat(items['price'].split(/[^0-9]+/).join('.').slice(1));
+	//transfer values from string to float number:
+	//first remove '$' symbol from '$10.99' and
+	//then transfer result to the float
+	let price = parseFloat(items['price'].slice(1));
 	let tax = price * 0.1;
 	let totalPrice = tax + price;
 
-	div.append($('<p>').html(items['name']));
-	div.append($('<p>').html('Subtotal price: ' + items['price']));
-	div.append($('<p>').html('Tax: $' + tax.toFixed(2)));
-	div.append($('<p>').html('Total price: $' + totalPrice.toFixed(2)));
+	//add item's name to the card-title class
+	//add price, tax and total price to the card-text
+	$('.card-title').html(items['name']);
+	$('.card-text')
+		.html('Subtotal price: $' + price + 
+			'<br> Tax: $' + tax.toFixed(2) + 
+			'<br> Total price: $' + totalPrice.toFixed(2));
+
+	//when button 'Delete item' class .btn-danger clicked
+	//remove this item
+	$('.btn-danger').click(function () {
+		let indexOfElement = $('.card').index($(this).closest('.card'))
+		if (indexOfElement >= 0) orderItems.splice(indexOfElement, 1);
+		$(this).closest('.card').remove();
+	});
 }
+
+//to make a form for the user 
+//to enter their Shipping Information
+function shippingInformation () {
+	//no scrolling background
+	$('body').addClass('activ');
+	//add shipping info block
+	$('.main')
+		.append($('<div>')
+		.addClass('container shipping-info'));
+	//add input form for shipping info
+	$('<div>')
+		.addClass('input-form')
+		.appendTo('.shipping-info');
+	//add label & input for name
+	$('.input-form')
+		.append($('<label [for="name-input" class="input-label"]>')
+		.text('Input name'), $('<input [ type="text" class="form-control" id="name-input" placeholder="Name" ]>'));
+	//add label & input for email
+	$('.input-form')
+		.append($('<label [for="form-control" class="input-label"]>')
+		.text('Email address'), $('<input [type="email" class="form-control" id="form-control" placeholder="name@example"]>'));
+	//add submit button
+	$('.input-form')
+		.append($('<button [type="button" class="btn btn-secondary input-form__button"]>')
+		.text('Submit'));
+
+
+	let nameSubmit;//for name from input
+	let emailSubmit;//for email from input
+	//add event listener to the name input form
+	$('#name-input').change(function(){
+		nameSubmit = $('#name-input').val();
+	});
+	//add event listener to the email input form
+	$('#form-control').change(function(){
+		emailSubmit = $('#form-control').val();
+		console.log(nameSubmit);
+		console.log(emailSubmit);
+	});
+
+	//add event listener for submit button
+	$('.input-form__button').click(() => submitItems(nameSubmit, emailSubmit));
+}
+
+//calculates subtotal price of goods
+function subtotalPrice(resultItems) {
+	return resultItems.reduce((sPrice, els) => 
+		sPrice += parseFloat(els['price'].slice(1))
+	, 0).toFixed(2);
+}
+
+//run when submit button pressed
+function submitItems (nameSubmit, emailSubmit) {
+	//removes all items from shopping card
+	$('.cart-items').empty();
+	//makes background scrollable again
+	$('body').removeClass('activ');
+	//removes a form where the user 
+	//entered their Shipping Information
+	$('.shipping-info').remove();
+	
+	//shows name, email, number of goods, subtotal price 
+	// and taxes
+	alert(`The order has been sent with data: \nName of customer: ${nameSubmit}\nEmail of customer: ${emailSubmit}\nThere are ${orderItems.length} elements in shopping list\nwith subtotal price $${subtotalPrice(orderItems)}\nand total tax $${(subtotalPrice(orderItems) * 0.1).toFixed(2)}\n\nThank you for shopping!`);
+	//empty all goods in the shopping card
+	orderItems = [];
+}
+
+//add event listener for 'Bye'-button on Shopping Cart
+$('.col-sm-3 button').click(shippingInformation);
